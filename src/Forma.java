f73471a37e161f3df778eeb1e75ab3f5b27f837e
Renato <renatoacf@gmail.com>
2023-04-02 09:34:58 -0300

    Exemplo com herança.

---
+++ src/Forma.java
@@ -0,0 +1,20 @@
+public class Forma {
+
+    private double area;
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public Forma() {}
+
+    public double calcularArea() {
+        return area;
+    }
+
+
+}
